086df9fcacc64e44d10b45ac560321c9ca71f36d
JaznanOfficial <jaznanofficial@gmail.com>
2021-10-29 19:03:52 +0600

    homepage done

---
+++ src/Components/ExtraSection-2/ExtraTwo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ExtraTwo = () => {
+    return (
+        <div>
+        <div class="embed-responsive embed-responsive-16by9">
+        <iframe class="embed-responsive-item" width="100%" height="409" src="https://www.youtube.com/embed/fN21oOdni_c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+        </div>
+    );
+};
+
+export default ExtraTwo;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ExtraOne from '../ExtraSection-1/ExtraOne';
+import ExtraTwo from '../ExtraSection-2/ExtraTwo';
 
 import Header from '../Header/Header';
 
@@ -11,6 +12,7 @@ const Home = () => {
             <Header></Header>
             <Services></Services>
             <ExtraOne></ExtraOne>
+            <ExtraTwo></ExtraTwo>
 
         </div>
     );
