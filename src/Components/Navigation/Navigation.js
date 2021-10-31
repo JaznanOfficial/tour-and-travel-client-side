9229d98bb0108afb4088cce2cb64d9bc777293a1
JaznanOfficial <jaznanofficial@gmail.com>
2021-10-31 07:40:07 +0600

    some changes

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -6,7 +6,8 @@ import logo from './travel.jpg'
 import useAuth from '../../Hooks/useAuth'
 
 const Navigation = () => {
-  const {user,logOut } = useAuth();
+  const { user, logOut } = useAuth();
+  console.log(user);
     return (
         <div>
         <Navbar bg="dark" variation='light' expand="lg">
@@ -14,12 +15,15 @@ const Navigation = () => {
           <Navbar.Brand href="#home"><img src={logo} alt="" width='80px' /></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
+            <Nav className="ms-auto d-flex align-items-center justify-content-center">
               <Link to='/home' className='navigation-link'>Home</Link>
               <Link to='/my-booked' className='navigation-link'>My Booked</Link>
-              <Link to='/addNewService' className='navigation-link'>Add a Service</Link>
+              
                 {user?.email ?
-                  <div className='d-flex flex-row justify-content-center align-items-center'> <h5 className='text-light'> <i className="fas fa-user-tie text-light bold"></i> {user.displayName}</h5> &nbsp; &nbsp; <Button variant="outline-light" onClick={logOut}>Log Out</Button> </div>
+                  <div className='d-flex flex-row justify-content-center align-items-center'>
+                  <Link to='/addNewService' className='navigation-link'>Add a Service</Link>
+                  <img src={user.photoURL} alt="" width='30px'/>&nbsp; &nbsp;
+                    <h5 className='text-light'>  {user.displayName}</h5> &nbsp; &nbsp; <Button variant="outline-light" onClick={logOut}>Log Out</Button> </div>
                   :
                   <Link to='/login' className='navigation-link'><Button variant="outline-light">Log In</Button></Link>}
                 
