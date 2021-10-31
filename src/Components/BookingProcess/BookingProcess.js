e3fb2ca3cac0433ed648f4b4d879be7e261d7ea3
JaznanOfficial <jaznanofficial@gmail.com>
2021-10-31 06:48:34 +0600

    auto name added

--- src/Components/BookingProcess/BookingProcess.js
+++ src/Components/BookingProcess/BookingProcess.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { Button, Card, Form } from "react-bootstrap";
 import { useParams } from "react-router";
+import useAuth from "../../Hooks/useAuth";
 
 const BookingProcess = () => {
+  const { user } = useAuth();
     const params = useParams();
-
     const { bookingId } = params;
-    console.log(bookingId);
     const [booking, setBooking] = useState([]);
     useEffect(() => {
         fetch("https://floating-brushlands-96149.herokuapp.com/places")
@@ -14,18 +14,25 @@ const BookingProcess = () => {
             .then((data) => setBooking(data[`${bookingId}` - 1]));
     }, []);
     const { img, PlaceName, CityName, price, PlaceId, placeDetails } = booking;
+
+
+  const handleBookingProcess = (e) => {
+    e.preventDefault()
+      console.log(PlaceName,CityName);
+    }
+  
     return (
         <div className="d-lg-flex justify-content-center mx-auto w-75 align-items-center">
             <div className='w-50 me-5 my-3'>
             <Form>
             <Form.Group className="mb-3" >
               <Form.Label>Your Name</Form.Label>
-              <Form.Control type="name" placeholder="Your Name" />
+              <Form.Control type="name" placeholder="Your Name" value = {user?.displayName} onChange={()=>{}} />
             </Form.Group>
           
             <Form.Group className="mb-3">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="email" />
+              <Form.Control type="email" placeholder= "Your Email" value= {user?.email} onChange={()=>{}}  />
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>Address</Form.Label>
@@ -36,7 +43,7 @@ const BookingProcess = () => {
               <Form.Control type="number" placeholder="Your Contact Number" />
             </Form.Group>
             
-            <Button variant="primary" type="submit">
+            <Button onClick={handleBookingProcess} variant="primary" type="submit">
               Submit
             </Button>
           </Form>
